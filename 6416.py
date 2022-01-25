48f452538c9e57a07392e203b0fd9beec42cc32a
LeeTaeHoon97 <dlxogns1004@gmail.com>
2022-01-26 01:33:09 +0900

    풀이완료
    풀이완료

--- 6416.py
+++ 6416.py
@@ -1,57 +1,79 @@
 import sys
+
 input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 9)
+from collections import defaultdict
 
 """
 dict을 key=간선의 도착 노드, val= 간선의 출발노드
 """
 
+# def dfs(start,graph):
+#     stack=list(start)
+#     visit=[]
+#     while stack:
+#         node = stack.pop()
+#         if node in graph:
+#             if node in visit:
+#                 return False
+#             visit.extend(graph[node])
+#             stack.extend(graph[node])
+#     return True
+
+
 if __name__ == '__main__':
+    def dfs(root):
+        stack = [root]
+        visited = set([root])
+        while stack:
+            node = stack.pop()
+            if node in graph:
+                for n in graph[node]:
+                    if n in visited:
+                        return False
+                    stack.append(n)
+                    visited.add(n)
+        return True
 
 
-    toggle=True
-    toggle2=True
-    is_tree=True
-    k=1
-    while True:
-        graph = dict()
-        while True :
+    toggle = True
+    # is_tree=True
+    k = 0
+    while toggle:
+        graph = defaultdict(list)
+        nodes = set()
+        child = set()
+        k += 1
+        toggle2 = True
+        while toggle2:
             # temp = list(map(int, input().split()))
-            temp=[int(i) for i in input().rstrip().split()]
-            while len(temp)>0:
-                s=temp.pop(0)
-                e=temp.pop(0)
-                if s<0 and e<0:
-                    toggle=False
-                    toggle2=False
+            temp = [int(i) for i in input().split()]
+            if len(temp) == 0:
+                continue
+            if temp[0] == -1 and temp[1] == -1:
+                sys.exit(0)
+            nodes.update(temp)
+            for i in range(0, len(temp), 2):
+                if temp[i] == 0 and temp[i + 1] == 0:
+                    toggle2 = False
                     break
-                if s==0 and e==0:
-                    toggle2=False
-                    break
-                if e not in graph:
-                    graph[e]=[]
-                graph[e].append(s)
-            if toggle2==False:
-                break
-        z=set()
+                if temp[i] not in graph:
+                    graph[temp[i]] = []
+                graph[temp[i]].append(temp[i + 1])
+                child.add(temp[i + 1])
+        if len(graph) == 0:
+            print(f'Case {k} is a tree.')
+            continue
+            # is_tree=True
+        root = nodes - child
+        root.remove(0)
+        # 루트가 두개이상일경우
+        if len(root) != 1:
+            print(f'Case {k} is not a tree.')
+            continue
 
-        print(set().union(set(i) for i in graph.values()]))
-        for key in graph:
-            if len(graph[key])!=1:
-                is_tree=False
-                break
-            idx=graph[key][0]
-            if idx in graph and graph[idx][0]==key:
-                is_tree=False
-                break
+        # if not dfs(root,graph):
+        #     is_tree=False
 
-        if toggle==False:
-            break
-
-        if is_tree:
-            print(f"Case {k} is a tree.")
-        else:
-            print(f"Case {k} is not a tree.")
-        k+=1
-        toggle2=True
-        is_tree=True
+        print(f'Case {k} is a tree.' if dfs(root.pop()) else f'Case {k} is not a tree.')
 
