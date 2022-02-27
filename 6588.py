cb6cfd4d9ecf399e1b353f22054278d7a1178594
LeeTaeHoon97 <dlxogns1004@gmail.com>
2022-02-27 22:58:03 +0900

    풀이중(2)

--- 6588.py
+++ 6588.py
@@ -16,24 +16,28 @@ if __name__ == '__main__':
                 tmp[t]=0
 
         return [idx for idx,val in enumerate(tmp) if val==1]
-
+    plst=[[0,0] for i in range(100001)]
     while True:
         n=int(input())
         if n==0:
             break
-        lst=get_prime(n)
-        left,right=0,len(lst)-1
+        if plst[n]!=[0,0]:
+            print(f"{n} = {plst[n][0]} + {plst[n][1]}")
+        else:
+            lst=get_prime(n)
+            left,right=0,len(lst)-1
 
-        while left<=right:
-                if lst[left]+lst[right]==n:
-                    print(f"{n} = {lst[left]} + {lst[right]}")
-                    break
-                elif lst[left]+lst[right]>n:
-                    right-=1
-                else :
-                    left+=1
+            while left<=right:
+                    if lst[left]+lst[right]==n:
+                        print(f"{n} = {lst[left]} + {lst[right]}")
+                        plst[n]=[lst[left],lst[right]]
+                        break
+                    elif lst[left]+lst[right]>n:
+                        right-=1
+                    else :
+                        left+=1
 
-        if left>=len(lst) or right<0:
-            print("Goldbach's conjecture is wrong.")
+            if left>=len(lst) or right<0:
+                print("Goldbach's conjecture is wrong.")
 
 
