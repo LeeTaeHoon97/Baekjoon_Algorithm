84ebcab8caaeb83e45a4e13a41a8f6ee6ddf9dae
LeeTaeHoon97 <dlxogns1004@gmail.com>
2022-03-06 19:46:44 +0900

    풀이완료

--- 15684.py
+++ 15684.py
@@ -2,9 +2,6 @@ import sys
 
 input = sys.stdin.readline
 
-from itertools import combinations
-
-import copy
 
 """
 처음에 입력받은 가로선을 추가
@@ -16,92 +13,63 @@ import copy
 ----------------------------시간초과
 두번째 원인 : 경우의수를 구할때 전체 경우의수를 구해버림
 ----------------------------시간초과
-세번째 원인 : 자체구현한 combination 알고리즘이 엄청난 시간을 소비함
+세번째 원인 : 자체구현한 combination 함수가 엄청난 시간을 소비함
+----------------------------시간초과
+네번째 원인 : 경우의수를 구하고 그 경우의수를 반복문을 도는것과.
+재귀적으로 바로 탐색해 나가는것은 속도를 약 절반으로 줄일수 있다.
+----------------------------시간초과
+다섯번째 원인 : 사다리의 위치를 담은 의미없는 stack을 사용한 부분에서 시간낭비
+----------------------------시간초과
+여섯번째 원인 : dfs의 if문 사용에 논리문제로 인한 시간낭비 발생
 """
-def show(board):
-    for i in board:
-        print(i)
-    print("----------------")
 
-def combi(idx,goal,cnt,stack=[]):
-    global lst,visit,comb
-    def make_arr(l):
-        tmp=[]
-        for i in l:
-            tmp.append(i)
-        return tmp
-    if cnt==goal:
-        comb.append(make_arr(stack))
+def dfs(idx,cnt):
+    global lst,visit,ans
+
+    if cnt>=ans:
+        return
+    if simul():
+        ans=cnt
         return
 
     for i in range(idx,len(lst)):
-        if not visit[i]:
-            visit[i]=1
-            stack.append(lst[i])
-            combi(i,goal,cnt+1,stack)
-            stack.pop()
-            visit[i]=0
+        y,x=lst[i]
+        if not visit[y][x]:
+            visit[y][x]=1
+            dfs(i+1,cnt+1)
+            visit[y][x]=0
 
-def simul(branch,cnt):
-    global Min
-    if cnt>Min:
-        return
-    b_board=copy.deepcopy(branch_board)
-    for temp in branch:
-        if temp!=[]:
-            y,x=temp
-            b_board[y][x]=1
-
-    # print(branch)
-    # show(b_board)
-
+def simul():
     for start in range(1,n+1):
         end=start
         for y in range(1,h+1):
-            if b_board[y][start-1]==1:
+            if visit[y][start-1]==1:
                 start-=1
-            elif b_board[y][start]==1:
+            elif visit[y][start]==1:
                 start+=1
         if end!=start:
-            return
-    if cnt<Min:
-        Min=cnt
-    return
+            return False
+    return True
 
 if __name__ == '__main__':
 
     n,m,h=[int(i) for i in input().split()]
-    Min=sys.maxsize
-    branch_board=[[-1 for i in range(n)]]+[[-1]+[0 for _ in range(n-1)]+[-1]for _ in range(h)]
-    branch=[]    #입력된 가지
+    ans=4
+    visit = [[0 for i in range(n+1)]for i in range(h+1)]
+
     for _ in range(m):
         y,x=[int(i) for i in input().split()]
-        branch.append([y,x])
-        branch_board[y][x]=1
-    if len(branch)==0:
+        visit[y][x]=1
+
+    if m==0:
         print('0')
     else:
         lst = []
         for i in range(1,h+1):
             for j in range(1,n):
-                if not branch_board[i][j]:
+                if not visit[i][j]:
                     lst.append([i,j])
 
-        comb=[]
-        combs=[]                        #결과적으로 combination이 저장될 리스트
-        visit=[0 for i in range(len(lst))]
-
-        for i in range(4):
-            combi(0, i, 0)
-            combs.append(comb)
-            comb=[]
-        for j in combs:
-            for z in j:
-                simul(branch+list(z),len(list(z)))
-                if Min==0:
-                    break
-        if Min>3:
-            print("-1")
-        else:
-            print(Min)
+        dfs(0,0)
+        print(ans if ans<4 else -1)
 
